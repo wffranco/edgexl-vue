0d1d93154c01e4a209cec1fec44eb257e0b547b9
Willem Franco <willemfranco@gmail.com>
2021-06-16 22:31:18 -0500

    init

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    '@wffranco/eslint-config',
+    'plugin:vue/vue3-recommended',
+    '@vue/typescript',
+    '@wffranco/eslint-config/vue3',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        mocha: true,
+      },
+    },
+  ],
+};
